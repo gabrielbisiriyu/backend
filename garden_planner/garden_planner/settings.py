ba4e154cf2cf0307e1690e525cfce40420c45b37
gabriel <guyex1996@gmail.com>
2025-01-26 05:28:21 +0100

    switched back to db sqlite

--- garden_planner/garden_planner/settings.py
+++ garden_planner/garden_planner/settings.py
@@ -106,13 +106,14 @@ WSGI_APPLICATION = "garden_planner.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-'''   
+ 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 '''
 
@@ -122,6 +123,7 @@ DATABASES = {
     "default" : dj_database_url.parse(config("DATABASE_URL"))
 }
 
+'''  
 
    
 
