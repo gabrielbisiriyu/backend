7dbbfa24d57b867ae97c703d29d0f77387d1bb91
gabriel <guyex1996@gmail.com>
2025-02-02 21:21:09 +0100

    again

--- garden_planner/garden_planner/settings.py
+++ garden_planner/garden_planner/settings.py
@@ -173,6 +173,7 @@ MEDIA_URL = "media/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField" 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True 
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173","https://garden-energy.vercel.app"]
 CORS_ALLOW_METHODS = ["DELETE","GET","OPTIONS","PATCH","POST","PUT"]
 #LOGOUT_REDIRECT_URL = 'login'  
 #LOGIN_URL = 'login'    
